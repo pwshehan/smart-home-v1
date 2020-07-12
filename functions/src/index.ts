5241d4dfd4bb27be5a653853a568afa4685e7eba
Shehan Pramitha <pwshehan@gmail.com>
2020-07-12 22:48:33 +0530

    lights init

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,8 +2,16 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as express from "express";
 import * as bodyParser from "body-parser";
+// import * as serviceAccount from "../";
 
-admin.initializeApp(functions.config().firebase);
+var serviceAccount = require("../keys/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://smarthomev1-381ab.firebaseio.com",
+});
+
+// admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
 const app = express();
@@ -17,3 +25,69 @@ export const webApi = functions.https.onRequest(main);
 app.get("/warm", (req, res) => {
   res.send("Calentando para la pelea");
 });
+
+// app.post("/fights", async (request, response) => {
+//   try {
+//     const { winner, loser, title } = request.body;
+//     const data = {
+//       winner,
+//       loser,
+//       title,
+//     };
+//     const fightRef = await db.collection("fights").add({ ...data });
+//     const fight = await fightRef.get();
+
+//     response.json({
+//       id: fightRef.id,
+//       data: fight.data(),
+//     });
+//   } catch (error) {
+//     response.status(500).send(error);
+//   }
+// });
+
+app.put("/lights/:id", async (request, response) => {
+  try {
+    const lightId = request.params.id;
+    const execer = request.body.execer;
+
+    if (!lightId) throw new Error("id is blank");
+
+    if (!execer) throw new Error("Title is required");
+
+    // const data = {
+    //   title,
+    // };
+    // await db.collection("lights").doc(lightId).set(data, { merge: true });
+    const cityRef = db.collection("lights").doc(lightId);
+    const doc = await cityRef.get();
+    if (!doc.exists) {
+      console.log("No such document!");
+      throw new Error("id not found");
+    } else {
+      console.log("Document data:", doc.data());
+      response.json({
+        id: lightId,
+        data: doc.data,
+      });
+    }
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+// app.delete("/fights/:id", async (request, response) => {
+//   try {
+//     const fightId = request.params.id;
+
+//     if (!fightId) throw new Error("id is blank");
+
+//     await db.collection("fights").doc(fightId).delete();
+
+//     response.json({
+//       id: fightId,
+//     });
+//   } catch (error) {
+//     response.status(500).send(error);
+//   }
+// });
